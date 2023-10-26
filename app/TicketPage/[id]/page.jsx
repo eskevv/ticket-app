38d9376dd286afd123fa9fd95c33d884dc4d4582
eskevv <hkeven89@gmail.com>
2023-10-26 12:29:13 -0400

    deploy

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -2,7 +2,7 @@ import EditTicketForm from "@/app/(components)/EditTicketForm";
 
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/Tickets/${id}`, {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
       cache: "no-store",
     });
 
