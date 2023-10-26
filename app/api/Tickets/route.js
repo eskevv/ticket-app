2fab637ffd38d7dcf40f8764dc80e4b964006fae
eskevv <hkeven89@gmail.com>
2023-10-26 11:00:59 -0400

    deploy

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,11 +1,11 @@
-import Ticket from "@/app/models/Ticket";
+import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const tickets = await Ticket.find();
+    const all_tickets_from_db = await Ticket.find();
 
-    return NextResponse.json({ tickets }, { status: 200 });
+    return NextResponse.json({ tickets: all_tickets_from_db }, { status: 200 });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: "Error", err }, { status: 500 });
