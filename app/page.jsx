2adf46742c60ef3ae68bda9185b5b174bb9dedf1
eskevv <hkeven89@gmail.com>
2023-10-26 13:36:53 -0400

    deploy

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
   try {
-    const res = await fetch("api/Tickets", {
+    const res = await fetch("/api/Tickets", {
       cache: "no-store",
     });
 
